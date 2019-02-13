e9d750f88a83a538121ebcada3e0f01eb52dcd10
neroark <ns_ark@hotmail.com>
2019-02-13 21:59:03 +0100

    feat: add burger ingredient

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Aux from "../../hoc/Aux2"
+
+
+const Burger = (props) => {
+    return(
+        <Aux>
+            Burger comp
+        </Aux>
+    )
+}
+
+export default Burger;
---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import Aux from "../../../hoc/Aux2";
+import classes from "./BurgerIngredient.css"
+
+const BurgerIngredient = (props)=>{
+    let ingredient = null;
+    const {type} = props
+
+    switch(type){
+        case('bread-bottom'):
+        ingredient = <div className={classes.BreadBottom}></div>;
+        break;
+
+        case('bread-top'):
+        ingredient = (
+            <div className={classes.BreadTop}>
+                <div className={classes.Seeds1}></div>
+                <div className={classes.Seeds2}></div>                
+            </div>
+        );
+        break;
+
+        case('meat'):
+        ingredient = <div className={classes.Meat}></div>;
+        break;
+
+        case('salad'):
+        ingredient = <div className={classesSalad}></div>;
+        break;
+
+        case('bacon'):
+        ingredient = <div className={classes.Bacon}></div>;
+        break;
+
+        case('cheese'):
+        ingredient = <div className={classes.Cheese}></div>;
+        break;
+
+    }
+    
+}
+
+export default BurgerIngredient;
