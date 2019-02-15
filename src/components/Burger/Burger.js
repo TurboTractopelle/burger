6b2acb90ffeefd4d5a4d83971a3fa059f302c832
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-02-15 13:47:58 +0100

    feat: burger state (11)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,16 +3,26 @@ import classes from "./Burger.css"
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
-    console.log(props)
+
+    console.log(props.ingredients)
+    // { salad: 1, bacon: 1, cheese: 2, meat: 2 }
+
+    //const ItemIngredients = Object.keys(props.ingredients)
+    // [ "salad", "bacon", "cheese", "meat" ]
+
+    const modifiedIngredients = Object.keys(props.ingredients).reduce((a,k,i)=>{
+        a = [...a, ...Array(props.ingredients[k]).fill(k)]
+        return a
+    },[])
+    // [ "salad", "bacon", "cheese", "cheese", "meat", "meat" ]
+    
     return(
         <div className={classes.burger}>
-            <BurgerIngredient type="bread-top"/>          
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="bacon"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="bread-bottom"/>            
+        {
+            modifiedIngredients.map((item,i)=>{
+            return <BurgerIngredient type={item} key={item+i} />
+        })
+        }
         </div>
     )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,20 @@ import Aux from '../../hoc/Aux2'
 import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component{
 
+    state ={
+        ingredients:{
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    }
 
     render(){
-        return(
+        const {ingredients} = this.state
+    return(
             <Aux>
-                <Burger />
+                <Burger ingredients={ingredients} />
                 <div>Build Controls</div>
             </Aux>
         )
