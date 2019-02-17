420fe566582fd7747afc4173b078dc285a201262
neroark <ns_ark@hotmail.com>
2019-02-17 16:01:47 +0100

    feat: add Logo (26)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux2';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import classes from './Layout.css';
 
 const Layout =(props)=>{
     return(
         <Aux>
-            <div>
-                layout, sideDrawer, Backdrop
-            </div>
+            <Toolbar />
             <main className={classes.content}>
                 {props.children}
             </main>
