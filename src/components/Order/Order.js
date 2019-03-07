3b138aedef1e5481e67bd42612431c38e47e0674
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-03-07 17:45:48 +0100

    feat: add order props

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,11 +3,22 @@ import classes from "./Order.css"
 
 const Order = (props)=>{
 
-    console.log(props)
+    console.log(props.ingredients)
+    // { bacon: 1, cheese: 0, meat: 1, salad: 5 }
+    // [  [["bacon"], [1]],   ...  ]
+
+    const modifiedIngredients = Object.entries(props.ingredients)
+
     return(
     <div className={classes.Order}>
-        <p>Ingredients: {props.ingredients}</p>
-        <p>Price: <strong>{props.price}</strong></p>
+
+            Ingredients: 
+            <ul>
+                {modifiedIngredients.map((k,i)=> <li key={i}><b>{k[0]}</b> ({k[1]})</li>)}
+            </ul>
+        
+
+        <p>Price: <strong>{props.price}$</strong></p>
     </div>)
 }
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import Order from "../../components/Order/Order"
+import axios from "../../axios-order"
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler"
 
 class Orders extends Component{
+
+state={orders:[], loading: true}
+
+    componentDidMount(){
+        axios.get("/orders.json")
+        .then(res => {
+            const fetchOrders = [];
+
+            for (let key in res.data){
+                fetchOrders.push( {...res.data[key], id:key} )
+            }
+            this.setState(prevState => ({...prevState, orders: fetchOrders, loading:false }))
+
+        })
+        .catch(err => console.log(err))
+        
+    }
 
 render(){
     return(
         <div>
-            <Order />
-            <Order />            
-            <Order />               
+            {this.state.orders.map( item=>{
+               return <Order key={item.id} {...item} />
+            })}             
         </div>
     )
 }
 
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
