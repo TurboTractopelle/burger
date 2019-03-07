2dc7f48e8a51f31d00529c6668e9484022c5be24
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-03-07 13:36:34 +0100

    feat: add orders page 12.8

---
+++ src/components/Order/Order.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from "./Order.css"
+
+const Order = (props)=>{
+
+    return(
+    <div className={classes.Order}>
+        <p>Ingredients: {props.ingredients}</p>
+        <p>Price: <strong>{props.price}</strong></p>
+    </div>)
+}
+
+export default Order
