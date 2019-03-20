b392d9d541828ab3168b4125e8bb2e83c6fc7d86
neroark <ns_ark@hotmail.com>
2019-03-20 16:14:14 +0100

    feat: add custom input 13.3

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './ContactData.css'
 import Button from "../../../components/UI/Button/Button"
+import Input from "../../../components/UI/Input/Input"
 
 class ContactData extends Component {
 
@@ -18,10 +19,12 @@ class ContactData extends Component {
             <div className={classes.ContactData}>
                 <h4>Your data</h4>
                 <form>
-                    <input type="text" name="name" placeholder="your name" />
-                    <input type="email" name="email" placeholder="your email" />
-                    <input type="text" name="street" placeholder="your street" />
-                    <input type="text" name="postal" placeholder="your postal" />
+                    <Input inputType="input" type="input" name="name" placeholder="your name" />
+                    <Input inputType="input" type="email" name="email" placeholder="your email" />
+                    <Input inputType="input" type="text" name="street" placeholder="your street" />
+                    <Input inputType="input" type="text" name="postal" placeholder="your postal" />
+
+
                     <Button btnType="Success">ORDER</Button>
                 </form>
             </div>
