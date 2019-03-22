e1764ca2d54df70927bfef481b79b033f88334ad
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-03-22 16:01:25 +0100

    fix: link to comp

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,7 +11,7 @@ onCheckoutCancelledHandler= ()=> {
     this.props.history.goBack()
 }
 onCheckoutContinueHandler = ()=>{
-    this.props.history.push("/contact-data");
+    this.props.history.push( this.props.match.path + "/contact-data");
 }
 
 render() {
