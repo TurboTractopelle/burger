959ff935c19e63a200e486804508bb0277461cbb
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-03-26 13:17:43 +0100

    fix: order ingredients 17.8

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -50,7 +50,12 @@ switch(action.type){
         case actionTypes.SET_INGREDIENTS : 
         return {
             ...state, 
-            ingredients: action.ingredients
+            ingredients: {
+                salad: action.ingredients.salad,
+                bacon: action.ingredients.bacon,
+                cheese: action.ingredients.cheese,                
+                meat: action.ingredients.meat 
+            }
         }
 
         case actionTypes.FETCH_FAILED : 
