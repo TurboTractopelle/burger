cbb698cd95d8f1eae32b3ccfa4e30589fa9c25f9
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-03-26 16:24:43 +0100

    feat: checkout redirect on empty ingredient

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary"
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import ContactData from "./ContactData/ContactData"
 import {connect} from "react-redux"
 
@@ -15,17 +15,27 @@ onCheckoutContinueHandler = ()=>{
 }
 
 render() {
+
+
+    let summary = (<div>
+        <CheckoutSummary 
+        ingredients={this.props.ingredients}
+        totalPrice={this.props.totalPrice}
+        onCheckoutCancelled={this.onCheckoutCancelledHandler}
+        onCheckoutContinue={this.onCheckoutContinueHandler}/>
+        <Route 
+        path={this.props.match.path + "/contact-data"} 
+        component={ContactData}
+        /></div>)
+
+    if(Object.entries(this.props.ingredients).length === 0){
+        summary = <Redirect to="/" />
+    }
+
+
     return (
         <div>
-            <CheckoutSummary 
-                ingredients={this.props.ingredients}
-                totalPrice={this.props.totalPrice}
-                onCheckoutCancelled={this.onCheckoutCancelledHandler}
-                onCheckoutContinue={this.onCheckoutContinueHandler}/>
-            <Route 
-                path={this.props.match.path + "/contact-data"} 
-                component={ContactData}
-                />
+            {summary}
         </div>
     );
 }
